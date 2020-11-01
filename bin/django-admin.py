798e93e3f315ae6e048f23598439858cdac58c28
AnuragTimilsina <anurag.timilsina@gmail.com>
2020-11-01 12:34:47 +0545

    API uploaded

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alcohol_repels_bugs/Documents/programming/DRF/to_do_api/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
