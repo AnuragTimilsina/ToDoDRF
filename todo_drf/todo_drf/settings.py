4656fa50de311e566a4726e641df066cb7e6e3d9
AnuragTimilsina <anurag.timilsina@gmail.com>
2020-11-01 13:58:53 +0545

    White listed angular's url

--- todo_drf/todo_drf/settings.py
+++ todo_drf/todo_drf/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:8080",
+
+]
